46e843991164cf609ffee3e360278197a0922b25
Anatol Zubenko <anatoliizubenko@gmail.com>
2017-02-13 18:39:07 +0200

    UnitTests

---
+++ src/main/java/com/azu777/core/classwork/lesson3/MathFunc.java
@@ -0,0 +1,8 @@
+package com.azu777.core.classwork.lesson3;
+
+/**
+ * Created by azu on 2/13/2017.
+ */
+public class MathFunc {
+    public static int multiply(int a, int b){return a * b;}
+}
---
+++ src/test/java/com/azu777/core/classwork/lesson3/MathFuncTest.java
@@ -0,0 +1,16 @@
+package com.azu777.core.classwork.lesson3;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by azu on 2/13/2017.
+ */
+public class MathFuncTest {
+    @Test // JUnit framework annotation
+    public void multiplyTest(){
+        int a = 1, b = 2, expRes = 2;
+        Assert.assertEquals(expRes, MathFunc.multiply(a, b));
+    }
+}
